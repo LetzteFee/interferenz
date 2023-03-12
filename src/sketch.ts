bb6f639ef301c84c46a28467442f6461ee30b37a
LetzteFee <jannes.folkerts@protonmail.com>
2023-03-13 00:28:28 +0100

    added support for multiple senders and support for calling render function from console with different arguments

--- src/sketch.ts
+++ src/sketch.ts
@@ -1,51 +1,53 @@
+function render(n: number = -1, lamda: number = -1): void {
+  let anzahl_sender: number = 2;
+  let wellenlaenge_pixel: number = 30;
+  if(n > 0) anzahl_sender = n;
+  if(lamda > 0) wellenlaenge_pixel = lamda;
+
+  interface Point {
+    x: number,
+    y: number
+  }
+  function d(x1: number, y1: number, x2: number, y2: number): number {
+    return Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
+  }
+  function wellen_funktion(ort: number): number {
+    const faktor: number = 2 * Math.PI / wellenlaenge_pixel;
+    return Math.sin(ort * faktor);
+  }
+
+  let l: Point[] = [];
+  for (let i: number = 0; i < anzahl_sender; i++) {
+    l[i] = { x: random(width), y: random(height) };
+  }
+
+  let progress: number = 0;
+  let current_progress: number;
+
+  for (let i: number = 0; i < width; i++) {
+    current_progress = Math.round(i / width * 100);
+    if (progress !== current_progress) {
+      progress = current_progress;
+      console.log(`Progress: ${progress}%`);
+    }
+
+    for (let j: number = 0; j < height; j++) {
+      let sum: number = 0;
+      // alle mit mit einem bestimmten Wert subtrahieren, sodass l[0] eine maximale Amplitude besitzt
+      let subtrahent: number = (d(i, j, l[0].x, l[0].y) % wellenlaenge_pixel) - (wellenlaenge_pixel * 0.25);
+      for (let k = 0; k < l.length; k++) {
+        sum += wellen_funktion(d(i, j, l[k].x, l[k].y) - subtrahent);
+      }
+
+      stroke(Math.abs(sum / l.length) * 255);
+      point(i, j);
+    }
+  }
+}
 function setup(): void {
   createCanvas(800, 800);
   background(128);
   noFill();
-
-  let inp: any = createInput("Render", "button");
-  inp.mouseClicked(function(): void {
-    interface Point {
-      x: number,
-      y: number
-    }
-    const wellenlaenge_pixel: number = 20;
-    function d(x1: number, y1: number, x2: number, y2: number): number {
-      return Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
-    }
-    function wellen_funktion(ort: number): number {
-      const faktor: number = 2 * Math.PI / wellenlaenge_pixel;
-      return Math.sin(ort * faktor);
-    }
-
-    let l1: Point = { x: random(width), y: random(height) };
-    let l2: Point = { x: random(width), y: random(height) };
-
-    let delta1: number;
-    let delta2: number;
-    let gang_unterschied: number;
-    let maximale_auslenkung: number;
-
-    let progress: number = 0;
-    let current_progress: number;
-
-    background(128);
-
-    for (let i: number = 0; i < width; i++) {
-      current_progress = Math.round(i / width * 100);
-      if (progress !== current_progress) {
-        progress = current_progress;
-        console.log(`Progress: ${progress}%`);
-      }
-      for (let j: number = 0; j < height; j++) {
-        delta1 = d(i, j, l1.x, l1.y);
-        delta2 = d(i, j, l2.x, l2.y);
-        gang_unterschied = Math.abs(delta1 - delta2);
-        maximale_auslenkung = 1 + wellen_funktion(wellenlaenge_pixel * 0.75 + gang_unterschied);
-
-        stroke(maximale_auslenkung * 127.5);
-        point(i, j);
-      }
-    }
-  });
+  strokeWeight(2);
+  createInput("Render", "button").mouseClicked(render);
 }
